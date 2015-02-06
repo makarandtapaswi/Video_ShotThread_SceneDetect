58044f4e3d59961fd9fa8726ea95f2666639fc28
Makarand Tapaswi <makarand.tapaswi@kit.edu>
2015-02-06 15:43:36 +0100

    add startup / first initialization scripts

---
+++ first_init.m
@@ -0,0 +1,39 @@
+% Performs some data download, compilation, and messaging operations on first initialization of the repository
+global VIDEOEVENTS
+
+% Create a file in PROJECTROOT/tmp/ to know whether it has been accessed before
+tmp_fname = 'tmp/first_init';
+if exist(tmp_fname, 'file')
+    run('ext/vlfeat/toolbox/vl_setup.m');
+    return;
+else
+    if ~isdir('tmp'), mkdir('tmp'); end
+    fid = fopen(tmp_fname, 'w');
+    fprintf(fid, 'Created temporary file on %s\n', date);
+    fclose(fid);
+end
+clear tmp_fname
+
+%% data download
+fprintf(2, 'Sample Image Data: BBT S01E01 excerpt\n');
+fprintf('\nPlease download the following zip file (27.3MB):\n');
+fprintf('https://cvhci.anthropomatik.kit.edu/~mtapaswi/downloads/shot_thread_scene_detect_data/bbt_s01e01_excerpt.zip\n');
+fprintf('... and unzip it in the same folder so that sample images are accessible.\n');
+while ~exist('data/bbt_s01e01_excerpt', 'dir') || ~exist('data/bbt_s01e01_excerpt/bbt_s01e01_000000.jpg', 'file')
+    fprintf('Press any key to continue, once you are done.\n');
+    pause;
+end
+fprintf('checked. Good to go\n');
+
+%% VLFeat
+fprintf(2, '\n\nVLFeat\n');
+fprintf('http://www.vlfeat.org/install-matlab.html\n');
+fprintf('Please download and unpack the folder to "%s/ext/vlfeat" and make sure the file vl_setup.m exists in the "toolbox" folder.\n', VIDEOEVENTS.base_dir);
+vlsetup_fname = 'ext/vlfeat/toolbox/vl_setup.m';
+while ~exist(vlsetup_fname, 'file')
+    fprintf('Press any key to continue...\n');
+    pause;
+end
+% Setup to use VLFeat
+run('ext/vlfeat/toolbox/vl_setup.m');
+
---
+++ startup.m
@@ -0,0 +1,39 @@
+%% Initialize the StoryGraphs project
+% This file will be called automatically on starting Matlab in this directory.
+
+clear all;
+clc;
+close all;
+
+% Make some directories
+if ~exist('tmp/', 'dir'),           mkdir('tmp/'); end
+if ~exist('cache/', 'dir'),         mkdir('cache/'); end
+
+global VIDEOEVENTS;
+
+%% Working directory
+VIDEOEVENTS.base_dir = [fileparts(mfilename('fullpath')) '/'];
+VIDEOEVENTS.mex_dir = [VIDEOEVENTS.base_dir, 'utilities/mex/'];
+
+% Check first initialization
+first_init;
+
+%% C++ / OKAPI / TRACKING-LIB Binaries
+VIDEOEVENTS.binaries.render_to_html = [VIDEOEVENTS.base_dir, 'visualization/gen_html.py'];
+VIDEOEVENTS.binaries.render_template_folder = [VIDEOEVENTS.base_dir, 'visualization/render_templates/'];
+
+%% Repository folders
+addpath(genpath('utilities/'));
+addpath('scenes/');
+addpath('threading/');
+addpath('initializers/');
+addpath('visualization/');
+
+%% Go go go :)
+fprintf('=======================================================\n');
+fprintf(['Initialized Shot Threading & Scene Detection repository. Supported videos:', ...
+         '\n\t%20s : The Big Bang Theory S01E01', ...
+         '\n\t%20s : Buffy the Vampire Slayer S01E01', ...
+     '\n'], ...
+     'BBT(se, ep)', 'BUFFY(se, ep)');
+
