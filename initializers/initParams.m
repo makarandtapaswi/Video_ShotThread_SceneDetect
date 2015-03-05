a8aacd7452df9a8d5a17c312bc9b3a1f253f88a1
Makarand Tapaswi <makarand.tapaswi@kit.edu>
2015-03-05 18:03:41 +0100

    cleanup init params

--- initializers/initParams.m
+++ initializers/initParams.m
@@ -1,4 +1,3 @@
-function params = initParams(VideoStruct)
 % ALL PARAMETERS FILE
 
 %% Shot descriptors
@@ -6,21 +5,8 @@ function params = initParams(VideoStruct)
 params.descriptor.rgbhist.use = true;
 params.descriptor.rgbhist.bins3d = 6;
 
-% Gray histogram
-params.descriptor.grayhist.use = false;
-params.descriptor.grayhist.bins = 32;
-
-% GIST features
-params.descriptor.gist_params.imageSize = [576 1024]; % it works also with non-square images
-params.descriptor.gist_params.orientationsPerScale = [8 8 8 8];
-params.descriptor.gist_params.numberBlocks = 4;
-params.descriptor.gist_params.fc_prefilt = 4;
-params.descriptor.gist_params.feat_dim = 512; % only used to generate storage space for saving
-
 %% Shot Similarity Parameters
 % Shot similarity based on homography
 params.shot_similarity.allowable_movement = 1; % normalized movement of the corners to match images
 params.shot_similarity.num_matches = 20; % number of required SIFT matches
 params.shot_similarity.lookahead = 24; % default of the "range" easily parfor with 12x2
-
-end
